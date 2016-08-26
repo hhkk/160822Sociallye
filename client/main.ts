00b403daa08980743886f98352effc35dad15cd2
hhkk <hank.khang@gmail.com>
2016-08-25 23:33:25 -0400

    poststep5b

--- client/main.ts
+++ client/main.ts
@@ -5,11 +5,7 @@ import { AppComponent } from './app.component';
 import { APP_ROUTER_PROVIDERS } from './app.routes';
 
 bootstrap(AppComponent, [
-    disableDeprecatedForms(),
-<<<<<<< HEAD
-    provideForms(),
-    APP_ROUTER_PROVIDERS
-=======
-    provideForms()
->>>>>>> 6aa914201db6b6db42910272547620422409e83c
+  disableDeprecatedForms(),
+  provideForms(),
+  APP_ROUTER_PROVIDERS
 ]);
