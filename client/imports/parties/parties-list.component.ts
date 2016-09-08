1f9b2d7563020605af2be67229b3782f8e8fb186
hhkk <hank.khang@gmail.com>
2016-09-07 21:54:03 -0400

    poststep9

--- client/imports/parties/parties-list.component.ts
+++ client/imports/parties/parties-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import { Mongo } from 'meteor/mongo';
 import { LoginButtons } from 'angular2-meteor-accounts-ui';
+import { MeteorComponent } from 'angular2-meteor';
 
 import { Parties }   from '../../../both/collections/parties.collection';
 import { Party } from '../../../both/interfaces/party.interface';
@@ -10,18 +11,30 @@ import { PartiesFormComponent } from './parties-form.component';
 import template from './parties-list.component.html';
 
 @Component({
-    selector: 'parties-list',
-    template,
-    directives: [PartiesFormComponent, ROUTER_DIRECTIVES, LoginButtons]
+  selector: 'parties-list',
+  template,
+  directives: [PartiesFormComponent, ROUTER_DIRECTIVES, LoginButtons]
 })
-export class PartiesListComponent implements OnInit {
-    parties: Mongo.Cursor<Party>;
+export class PartiesListComponent extends MeteorComponent implements OnInit {
+  parties: Mongo.Cursor<Party>;
 
-    ngOnInit() {
-        this.parties = Parties.find();
-    }
+  constructor() {
+    super();
+  }
 
-    removeParty(party) {
-        Parties.remove(party._id);
-    }
+  ngOnInit() {
+    this.parties = Parties.find();
+
+    this.subscribe('parties', () => {
+      this.parties = Parties.find();
+    }, true);
+  }
+
+  removeParty(party) {
+    Parties.remove(party._id);
+  }
+
+  search(value: string) {
+    this.parties = Parties.find(value ? { location: value } : {});
+  }
 }
--- client/imports/parties/party-details.component.ts
+++ client/imports/parties/party-details.component.ts
@@ -1,53 +1,50 @@
-import { Component, NgZone, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ROUTER_DIRECTIVES } from '@angular/router';
 import { Tracker } from 'meteor/tracker';
 import { Meteor } from 'meteor/meteor';
+import { MeteorComponent } from 'angular2-meteor';
 
 import { Parties } from '../../../both/collections/parties.collection';
 import { Party } from '../../../both/interfaces/party.interface';
 
-
-
 import template from './party-details.component.html';
 
 @Component({
-    selector: 'party-details',
-    template,
-    directives: [ROUTER_DIRECTIVES]
+  selector: 'party-details',
+  template,
+  directives: [ROUTER_DIRECTIVES]
 })
+export class PartyDetailsComponent extends MeteorComponent implements OnInit {
+  partyId: string;
+  party: Party;
 
+  constructor(private route: ActivatedRoute) {
+    super();
+  }
 
-export class PartyDetailsComponent implements OnInit {
-    partyId: string;
-    party: Party;
+  ngOnInit() {
+    this.route.params
+      .map(params => params['partyId'])
+      .subscribe(partyId => {
+        this.partyId = partyId;
 
-    constructor(private route: ActivatedRoute, private ngZone: NgZone) {}
+        this.subscribe('party', this.partyId, () => {
+          this.party = Parties.findOne(this.partyId);
+        }, true);
+      });
+  }
 
-    ngOnInit() {
-        this.route.params
-            .map(params => params['partyId'])
-            .subscribe(partyId => {
-                this.partyId = partyId;
-
-                Tracker.autorun(() => {
-                    this.ngZone.run(() => {
-                        this.party = Parties.findOne(this.partyId);
-                    });
-                });
-            });
-    }
-
-    saveParty() {
+  saveParty() {
     if (Meteor.userId()) {
-        Parties.update(this.party._id, {
-            $set: {
-                name: this.party.name,
-                description: this.party.description,
-                location: this.party.location
-            }
-        });
+      Parties.update(this.party._id, {
+        $set: {
+          name: this.party.name,
+          description: this.party.description,
+          location: this.party.location
+        }
+      });
     } else {
       alert('Please log in to change this party');
     }
-    }
+  }
 }
